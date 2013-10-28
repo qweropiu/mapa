6457dcb0b35aa814342fcee61b02b17c8874848a
qweropiu <pikachu509@gmail.com>
2013-10-28 20:54:24 +0000

    add getdirections

--- lol.js
+++ lol.js
@@ -1,26 +1,28 @@
+var dirDisp;
+var directionsService = new google.maps.DirectionsService();
+var fromInput, toInput;
+
 function initialize() {
+  fromInput = /** @type {HTMLInputElement} */(document.getElementById('fromField'));
+  toInput = /** @type {HTMLInputElement} */(document.getElementById('toField'));
+  var inputs = [fromInput, toInput];
   var mapOptions = {
     center: new google.maps.LatLng(40.7143528, -74.0059731),
     zoom: 10,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
-
-  var fromInput = /** @type {HTMLInputElement} */(document.getElementById('fromField'));
-  var toInput = /** @type {HTMLInputElement} */(document.getElementById('toField'));
-  var inputs = [fromInput, toInput];
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  dirDisp = new google.maps.DirectionsRenderer();
+  dirDisp.setMap(map);
 
   for (i = 0; i < 2; i++) {
     var input = inputs[i];
-
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
 
     marker = new google.maps.Marker({map: map});
 
-    google.maps.event.addListener(autocomplete, 'place_changed', (function(marker, autocomplete) {
+    google.maps.event.addListener(autocomplete, 'place_changed', (function(marker, autocomplete, input) {
       return function() {
         marker.setVisible(false);
         input.className = '';
@@ -41,8 +43,22 @@ function initialize() {
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
       }
-    })(marker, autocomplete));
+    })(marker, autocomplete, input));
   }
 }
+
+function calcRoute() {
+  var request = {
+      origin: fromInput.value,
+      destination: toInput.value,
+      travelMode: google.maps.DirectionsTravelMode.TRANSIT
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      dirDisp.setDirections(response);
+    }
+  });
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
